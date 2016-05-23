ce395f1d2dc79e742ac64dd08adae172b347ed56
Diogo Rato <diogo.rato.11@gmail.com>
2016-05-23 17:39:56 +0100

    Created WPF  to Question the Virtual Agent Created WPF to View the Story

---
+++ VirtualSuspect/VirtualSuspectUI/EventViewer.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VirtualSuspectUI {
+    /// <summary>
+    /// Interaction logic for EventViewer.xaml
+    /// </summary>
+    public partial class EventViewer : UserControl {
+
+        public EventViewer() {
+
+            InitializeComponent();
+
+        }
+
+        public EventViewer(uint ID, string action, string location, string time, List<string> agents, List<string> themes, List<string> manners, List<string> reasons, bool isActive) {
+
+            InitializeComponent();
+
+            EventIdLabel.Content = "Event ID: " + ID;
+            ActiveLabel.IsEnabled = isActive;
+
+            ActionTextBox.Text = action;
+            TimeTextBox.Text = time;
+            LocationTextBox.Text = location;
+
+            AgentTextBox.Text = CreateStringForList(agents);
+            ThemeTextBox.Text = CreateStringForList(themes);
+            ReasonTextBox.Text = CreateStringForList(reasons);
+            MannerTextBox.Text = CreateStringForList(manners);
+
+
+            
+        }
+
+        private string CreateStringForList(List<string> objects) {
+
+            string result = "";
+
+            switch (objects.Count) {
+                case 0:
+                    result = "none";
+                    break;
+                case 1:
+                    result = objects[0];
+                    break;
+                default:
+                    result += "{ ";
+                    for (int i = 0; i < objects.Count; i++) {
+                        result += objects[i];
+                        if (i < objects.Count - 1) {
+                            result += ", ";
+                        }
+                    }
+                    result += "}";
+                    break;
+            }
+
+            return result;
+
+        }
+
+    }
+}
