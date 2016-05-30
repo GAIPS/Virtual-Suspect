ccf75e5c4c16f10d500ba93bc23e4d15d665a093
Diogo Rato <diogo.rato.11@gmail.com>
2016-05-30 22:13:43 +0100

    Added Natural Language Module

---
+++ VirtualSuspect/VirtualSuspectNaturalLanguage/NaturalLanguageGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualSuspect.Query;
+
+namespace VirtualSuspectNaturalLanguage
+{
+    public static class NaturalLanguageGenerator{
+
+        public static string GenerateAnswer(QueryResult result) {
+
+            string answer = "";
+            
+            //Prefilter the Answer According to the MetaData
+
+            //If its a non-remember or denial (negative answer)
+            if(result.MetaData.ContainsKey("negative-answer")) {
+
+                switch(result.MetaData["negative-answer"]) {
+                    case "non-remember":
+                        answer += "I can't remember that.";
+                        break;
+                    case "denial":
+                        answer += "I don't want to talk about it.";
+                        break;
+                }
+
+            //If its a irrelevant (special answer)
+            }else if(result.MetaData.ContainsKey("negative-answer")) {
+                
+                switch(result.MetaData["special-answer"]) {
+                    case "irrelevant":
+                        answer += "I don't find that question relevant!";
+                        break;
+                }
+        
+            }
+
+            return answer;
+
+        }
+    }
+}
